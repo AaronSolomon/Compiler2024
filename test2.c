4e2f8f9a7500ecbece6e6961f9ae042898013660
Solomon <solomon@lilina.csie.ncnu.edu.tw>
2024-03-18 22:58:38 +0800

    fb2-5.l: C Language Cross-Reference

---
+++ test2.c
@@ -0,0 +1,5 @@
+int main() {
+    int a = 10;
+    char c = 'm';
+    return 0;
+}
---
+++ test3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    int b = 20;
+    int c;
+    c = a + b;
+    printf("%d\n", c);
+    return 0;
+}
+
+/*  Multi-line remarks
+ *  int d = 10;
+ *  int e = 20;
+ *  int f;
+ *  f = d + e;
+ */
